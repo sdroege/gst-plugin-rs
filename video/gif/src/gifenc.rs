c259d75a21113fa5e2cf016b7bddf2dadba5c063
Sebastian Dröge <sebastian@centricular.com>
2020-07-10 12:58:09 +0300

    video/gif: Don't set NON_DROPPABLE flag on buffers
    It doesn't make sense in this context and the flag only exists since
    1.12.

--- video/gif/src/gifenc.rs
+++ video/gif/src/gifenc.rs
@@ -408,7 +408,6 @@ impl GifEnc {
             {
                 let trailer_buffer = trailer_buffer.get_mut().unwrap();
                 trailer_buffer.set_pts(state.last_actual_pts);
-                trailer_buffer.set_flags(gst::BufferFlags::NON_DROPPABLE);
             }
 
             // Initialize the encoder again, to be ready for a new round without format change
