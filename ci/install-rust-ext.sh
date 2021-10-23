db30cfcc74247dbd9f33899215852d70948e759a
Sebastian Dröge <sebastian@centricular.com>
2021-10-23 10:37:55 +0300

    ci: Install cargo-c 0.9.4 until our minimum supported Rust version is 1.56

--- ci/install-rust-ext.sh
+++ ci/install-rust-ext.sh
@@ -3,4 +3,5 @@ source ./ci/env.sh
 set -e
 export CARGO_HOME='/usr/local/cargo'
 
-cargo install cargo-c
+# Install 0.9.4 until Rust 1.56 is the minimum supported version here
+cargo install cargo-c --version 0.9.4+cargo-0.56
