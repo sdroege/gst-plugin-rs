574e4f6b647e796c56a16ff85429cd6e6f60c7cc
Sebastian Dröge <sebastian@centricular.com>
2022-01-15 21:18:47 +0200

    Add SPDX-License-Identifier to all file headers

--- generic/file/src/file_location.rs
+++ generic/file/src/file_location.rs
@@ -1,3 +1,12 @@
+// Copyright (C) 2016-2017 Sebastian Dröge <sebastian@centricular.com>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// SPDX-License-Identifier: MIT/Apache-2.0
 use gst::glib;
 use url::Url;
 
--- video/rspng/src/pngenc/imp.rs
+++ video/rspng/src/pngenc/imp.rs
@@ -5,6 +5,8 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+//
+// SPDX-License-Identifier: MIT/Apache-2.0
 
 use std::{io, io::Write, sync::Arc};
 
