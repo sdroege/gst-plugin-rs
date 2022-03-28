sdroege/gst-plugin-rs

commit ab96219c19f9009de1d942c41adebd0dd1645750

uriplaylistbin: fix deadlock when shutting down
The probes on srcpad was not removed which was preventing the element to
reach the NULL state.

utils/uriplaylistbin/src/uriplaylistbin/imp.rs
@@ -607,6 +607,17 @@ impl Item {
             _ => panic!("invalid state: {:?}", inner.state),
         }
     }
+
+    fn sender(&self) -> crossbeam_channel::Sender<bool> {
+        let inner = self.inner.lock().unwrap();
+
+        match &inner.state {
+            ItemState::WaitingForStreamsynchronizerEos { sender, .. } => sender.clone(),
+            ItemState::WaitingForPads { sender, .. } => sender.clone(),
+            ItemState::Blocked { sender, .. } => sender.clone(),
+            _ => panic!("invalid state: {:?}", inner.state),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -932,6 +943,24 @@ impl ElementImpl for UriPlaylistBin {
             if let Err(e) = self.start(element) {
                 self.failed(element, e);
                 return Err(gst::StateChangeError);
+            }
+        }
+
+        if transition == gst::StateChange::PausedToReady {
+            let mut state_guard = self.state.lock().unwrap();
+            let state = state_guard.as_mut().unwrap();
+
+            // The probe callback owns a ref on the item and so on the sender as well.
+            // As a result we have to explicitly unblock all receivers as dropping the sender
+            // is not enough.
+            if let Some(item) = state.waiting_for_ss_eos.take() {
+                let _ = item.sender().send(false);
+            }
+            if let Some(item) = state.waiting_for_pads.take() {
+                let _ = item.sender().send(false);
+            }
+            if let Some(item) = state.blocked.take() {
+                let _ = item.sender().send(false);
             }
         }
 

commit 5dab4bc5028f4cf5b5bcacf7e3afb3bbfd1ebb9b

uriplaylistbin: handle waiting items no longer being set
ab96219c19f9009de1d942c41adebd0dd1645750 broke some assertions as
waiting items are now unset when shutting down the element.

utils/uriplaylistbin/src/uriplaylistbin/imp.rs
@@ -1232,7 +1232,10 @@ impl UriPlaylistBin {
                 return;
             }
 
-            let item = state.waiting_for_pads.clone().unwrap();
+            let item = match state.waiting_for_pads.as_ref() {
+                Some(item) => item.clone(),
+                None => return, // element is being shutdown
+            };
 
             // Parse the pad name to extract the stream type and its index.
             // We could get the type from the Stream object from the StreamStart sticky event but we'd still have
@@ -1529,7 +1532,11 @@ impl UriPlaylistBin {
             let mut state_guard = self.state.lock().unwrap();
             let state = state_guard.as_mut().unwrap();
 
-            let item = state.waiting_for_ss_eos.take().unwrap();
+            let item = match state.waiting_for_ss_eos.take() {
+                Some(item) => item,
+                None => return, // element is being shutdown
+            };
+
             let (topology, pending_pads, sender) = item.done_waiting_for_ss_eos();
             state.waiting_for_pads = Some(item);
 