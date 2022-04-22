d69b4edfbc8013c7ae411e650a656b7cbb0a53a4
Guillaume Desmottes <guillaume.desmottes@onestream.live>
2022-04-22 14:40:18 +0200

    uriplaylistbin: fix number of streaming items
    One more item than intended was allowed.

--- utils/uriplaylistbin/src/uriplaylistbin/imp.rs
+++ utils/uriplaylistbin/src/uriplaylistbin/imp.rs
@@ -1051,7 +1051,7 @@ impl UriPlaylistBin {
         }
 
         let n_streaming = state.streaming.len();
-        if n_streaming > MAX_STREAMING_ITEMS {
+        if n_streaming >= MAX_STREAMING_ITEMS {
             gst::log!(
                 CAT,
                 obj: element,
