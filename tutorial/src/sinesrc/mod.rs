e93544cfc6a42a2b6aab55f656b74fecefa4346f
Sebastian Dröge <sebastian@centricular.com>
2022-05-10 13:04:50 +0300

    tutorial/sinesrc: Use pushsrc as direct superclass in the glib::wrapper macro too

--- tutorial/src/sinesrc/mod.rs
+++ tutorial/src/sinesrc/mod.rs
@@ -15,7 +15,7 @@ mod imp;
 
 // The public Rust wrapper type for our element
 glib::wrapper! {
-    pub struct SineSrc(ObjectSubclass<imp::SineSrc>) @extends gst_base::BaseSrc, gst::Element, gst::Object;
+    pub struct SineSrc(ObjectSubclass<imp::SineSrc>) @extends gst_base::PushSrc, gst_base::BaseSrc, gst::Element, gst::Object;
 }
 
 // Registers the type for our element, and then registers in GStreamer under
