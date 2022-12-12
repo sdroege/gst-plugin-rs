289e8a08c31610fbf62056f8a156eb647e9c3a2a
Sebastian Dröge <sebastian@centricular.com>
2022-12-12 14:31:33 +0200

    webrtchttp: Remove unnecessary clippy warning override
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1009>

--- net/webrtchttp/src/whepsrc/imp.rs
+++ net/webrtchttp/src/whepsrc/imp.rs
@@ -445,10 +445,8 @@ impl WhepSrc {
                         // With tokio's spawn one does not have to .await the
                         // returned JoinHandle to make the provided future start
                         // execution. It will start running in the background
-                        // immediately when spawn is called. So silence the clippy
-                        // warning.
-                        #[allow(clippy::let_underscore_future)]
-                        let _ = RUNTIME.spawn(async move {
+                        // immediately when spawn is called.
+                        RUNTIME.spawn(async move {
                             /* Note that we check for a valid WHEP endpoint in change_state */
                             self_ref.whep_offer().await
                         });
