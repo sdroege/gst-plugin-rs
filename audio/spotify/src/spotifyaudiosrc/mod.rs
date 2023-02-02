fffecca6240ec7822287e42d14076bfba206a26d
Simon Himmelbauer <simfreak@deliciae.at>
2023-02-02 12:05:59 +0100

    spotifyaudiosrc: Support configurable bitrate
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1086>

--- audio/spotify/src/spotifyaudiosrc/mod.rs
+++ audio/spotify/src/spotifyaudiosrc/mod.rs
@@ -11,11 +11,42 @@ use gst::prelude::*;
 
 mod imp;
 
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Clone, Copy, glib::Enum)]
+#[repr(u32)]
+#[enum_type(name = "GstRsSpotifyBitrate")]
+enum Bitrate {
+    #[enum_value(name = "96 kbit/s", nick = "96")]
+    B96,
+    #[enum_value(name = "160 kbit/s", nick = "160")]
+    B160,
+    #[enum_value(name = "320 kbit/s", nick = "320")]
+    B320,
+}
+
+impl Default for Bitrate {
+    fn default() -> Self {
+        Self::B160
+    }
+}
+
+impl From<Bitrate> for librespot::playback::config::Bitrate {
+    fn from(value: Bitrate) -> Self {
+        match value {
+            Bitrate::B96 => Self::Bitrate96,
+            Bitrate::B160 => Self::Bitrate160,
+            Bitrate::B320 => Self::Bitrate320,
+        }
+    }
+}
+
 glib::wrapper! {
     pub struct SpotifyAudioSrc(ObjectSubclass<imp::SpotifyAudioSrc>) @extends gst_base::PushSrc, gst_base::BaseSrc, gst::Element, gst::Object, @implements gst::URIHandler;
 }
 
 pub fn register(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
+    #[cfg(feature = "doc")]
+    Bitrate::static_type().mark_as_plugin_api(gst::PluginAPIFlags::empty());
+
     gst::Element::register(
         Some(plugin),
         "spotifyaudiosrc",
