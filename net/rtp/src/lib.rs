c65b3429ad5bae209575bc90b78f39b84d468264
Sebastian Dröge <sebastian@centricular.com>
2023-06-07 12:05:00 +0300

    Use MPL as license specifier for plugins only requiring GStreamer < 1.20
    And use MPL-2.0 for all that require GStreamer 1.20 or newer. The new
    string is only allowed in 1.20 or newer and using it in older versions
    causes failure to load the plugin.
    
    All affected plugins are of course still MPL-2.0 licensed.
    
    Fixes https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/issues/374
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1235>

--- net/rtp/src/lib.rs
+++ net/rtp/src/lib.rs
@@ -31,7 +31,7 @@ gst::plugin_define!(
     env!("CARGO_PKG_DESCRIPTION"),
     plugin_init,
     concat!(env!("CARGO_PKG_VERSION"), "-", env!("COMMIT_ID")),
-    "MPL",
+    "MPL-2.0",
     env!("CARGO_PKG_NAME"),
     env!("CARGO_PKG_NAME"),
     env!("CARGO_PKG_REPOSITORY"),
