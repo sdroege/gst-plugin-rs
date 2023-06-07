de0f7a08fe0cd1aca8ecb12ec2315d5f04373e57
Mathieu Duponchelle <mathieu@centricular.com>
2023-06-08 01:50:59 +0200

    gstwebrtc-api: fix firefox errors about more than two stun servers
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1236>

--- net/webrtc/gstwebrtc-api/src/config.js
+++ net/webrtc/gstwebrtc-api/src/config.js
@@ -40,10 +40,7 @@ const defaultConfig = Object.freeze({
       {
         urls: [
           "stun:stun.l.google.com:19302",
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-          "stun:stun3.l.google.com:19302",
-          "stun:stun4.l.google.com:19302"
+          "stun:stun1.l.google.com:19302"
         ]
       }
     ]
