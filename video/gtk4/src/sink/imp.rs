755f021a30d21637851cc3ccd1fedf54725bd5b3
Sebastian Dröge <sebastian@centricular.com>
2023-09-12 10:19:37 +0300

    gtk4: Only support RGBA/RGB in the GL code path
    For all other component orderings a shader is necessary to re-order the
    components for what GTK expects.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1328>

--- video/gtk4/src/sink/imp.rs
+++ video/gtk4/src/sink/imp.rs
@@ -161,15 +161,27 @@ impl ElementImpl for PaintableSink {
                     Some(&["memory:SystemMemory", "meta:GstVideoOverlayComposition"][..]),
                     Some(&["meta:GstVideoOverlayComposition"][..]),
                 ] {
-                    let mut c = gst_video::video_make_raw_caps(&[
+                    const GL_FORMATS: &[gst_video::VideoFormat] =
+                        &[gst_video::VideoFormat::Rgba, gst_video::VideoFormat::Rgb];
+                    const NON_GL_FORMATS: &[gst_video::VideoFormat] = &[
                         gst_video::VideoFormat::Bgra,
                         gst_video::VideoFormat::Argb,
                         gst_video::VideoFormat::Rgba,
                         gst_video::VideoFormat::Abgr,
                         gst_video::VideoFormat::Rgb,
                         gst_video::VideoFormat::Bgr,
-                    ])
-                    .build();
+                    ];
+
+                    let formats = if features
+                        .as_ref()
+                        .map_or(false, |features| features.contains(&"memory:GLMemory"))
+                    {
+                        GL_FORMATS
+                    } else {
+                        NON_GL_FORMATS
+                    };
+
+                    let mut c = gst_video::video_make_raw_caps(formats).build();
 
                     if let Some(features) = features {
                         c.get_mut()
