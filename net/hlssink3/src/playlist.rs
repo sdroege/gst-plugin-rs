7f16ac3915046bdf28c64960092f32debaae1c8b
Seungha Yang <seungha@centricular.com>
2023-09-24 17:51:45 +0900

    hlssink3: Use sprintf for segment name formatting
    The zero-padded naming requirement is unnecessary. Use simple
    sprintf instead
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1306>

--- net/hlssink3/src/playlist.rs
+++ net/hlssink3/src/playlist.rs
@@ -7,15 +7,11 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use chrono::{DateTime, Utc};
-use gst::glib::once_cell::sync::Lazy;
 use m3u8_rs::{MediaPlaylist, MediaPlaylistType, MediaSegment};
-use regex::Regex;
 use std::io::Write;
 
 const GST_M3U8_PLAYLIST_V3: usize = 3;
 const GST_M3U8_PLAYLIST_V4: usize = 4;
-
-static SEGMENT_IDX_PATTERN: Lazy<regex::Regex> = Lazy::new(|| Regex::new(r"(%0(\d+)d)").unwrap());
 
 /// An HLS playlist.
 ///
@@ -158,120 +154,4 @@ impl Playlist {
 pub enum PlaylistRenderState {
     Init,
     Started,
-}
-
-/// A formatter for segment locations.
-///
-/// The formatting is based on a string that must contain the placeholder `%0Xd` where `X` is a
-/// the number of zero prefixes you want to have in the segment name. The placeholder is only
-/// replaced once in the string, other placements are not going to be processed.
-///
-/// # Examples
-///
-/// In this example we want to have segment files with the following names:
-/// ```text
-/// part001.ts
-/// part002.ts
-/// part003.ts
-/// part004.ts
-/// ```
-/// Then we can use the segment pattern value as `"part%03d.ts"`:
-///
-/// ```rust,ignore
-/// let formatter = SegmentFormatter::new("part%03d.ts").unwrap();
-/// assert_eq!(formatter.segment(1), "part001.ts");
-/// assert_eq!(formatter.segment(2), "part002.ts");
-/// assert_eq!(formatter.segment(3), "part003.ts");
-/// assert_eq!(formatter.segment(4), "part004.ts");
-/// ```
-pub struct SegmentFormatter {
-    prefix: String,
-    suffix: String,
-    padding_len: u32,
-}
-
-impl SegmentFormatter {
-    /// Processes the segment name containing a placeholder. It can be used
-    /// repeatedly to format segment names.
-    ///
-    /// If an invalid placeholder is provided, then `None` is returned.
-    pub fn new<S: AsRef<str>>(segment_pattern: S) -> Option<Self> {
-        let segment_pattern = segment_pattern.as_ref();
-        let caps = SEGMENT_IDX_PATTERN.captures(segment_pattern)?;
-        let number_placement_match = caps.get(1)?;
-        let zero_pad_match = caps.get(2)?;
-        let padding_len = zero_pad_match
-            .as_str()
-            .parse::<u32>()
-            .expect("valid number matched by regex");
-        let prefix = segment_pattern[..number_placement_match.start()].to_string();
-        let suffix = segment_pattern[number_placement_match.end()..].to_string();
-        Some(Self {
-            prefix,
-            suffix,
-            padding_len,
-        })
-    }
-
-    /// Returns the segment location formatted for the provided id.
-    #[inline]
-    pub fn segment(&self, id: u32) -> String {
-        let padded_number = left_pad_zeroes(self.padding_len, id);
-        format!("{}{}{}", self.prefix, padded_number, self.suffix)
-    }
-}
-
-/// Transforms a number to a zero padded string representation.
-///
-/// The zero padding is added to the left of the number which is converted to a string. For the
-/// case that the length number converted to string is larger than the requested padding, the
-/// number representation is returned and no padding is added. The length of the returned string is
-/// the maximum value between the desired padding and the length of the number.
-///
-/// # Examples
-///
-/// ```rust,ignore
-/// let padded_number = left_pad_zeroes(4, 10);
-/// assert_eq!(padded_number, "0010");
-/// ```
-#[inline]
-pub(crate) fn left_pad_zeroes(padding: u32, number: u32) -> String {
-    let numerical_repr = number.to_string();
-    let mut padded = String::with_capacity(padding.max(numerical_repr.len() as u32) as usize);
-    let pad_zeroes = padding as i32 - numerical_repr.len() as i32;
-    if pad_zeroes > 0 {
-        for _ in 0..pad_zeroes {
-            padded.push('0');
-        }
-    }
-    padded.push_str(&numerical_repr);
-    padded
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn segment_is_correctly_formatted() {
-        let formatter = SegmentFormatter::new("segment%05d.ts").unwrap();
-
-        assert_eq!("segment00001.ts", formatter.segment(1));
-        assert_eq!("segment00016.ts", formatter.segment(16));
-        assert_eq!("segment01827.ts", formatter.segment(1827));
-        assert_eq!("segment98765.ts", formatter.segment(98765));
-
-        let formatter = SegmentFormatter::new("part-%03d.ts").unwrap();
-        assert_eq!("part-010.ts", formatter.segment(10));
-        assert_eq!("part-9999.ts", formatter.segment(9999));
-    }
-
-    #[test]
-    fn padding_numbers() {
-        assert_eq!("001", left_pad_zeroes(3, 1));
-        assert_eq!("010", left_pad_zeroes(3, 10));
-        assert_eq!("100", left_pad_zeroes(3, 100));
-        assert_eq!("1000", left_pad_zeroes(3, 1000));
-        assert_eq!("987654321", left_pad_zeroes(3, 987654321));
-    }
 }
