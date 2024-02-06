cf5e7f6ed34a919878a3e6be67ea6be198856b3c
Nirbheek Chauhan <nirbheek@centricular.com>
2024-02-06 18:39:45 +0530

    rtspsrc2: Add some top-level documentation
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1425>

--- net/rtsp/src/rtspsrc/mod.rs
+++ net/rtsp/src/rtspsrc/mod.rs
@@ -8,6 +8,30 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+/**
+ * SECTION:element-rtspsrc2
+ *
+ * `rtspsrc2` is a from-scratch rewrite of the `rtspsrc` element to fix some fundamental
+ * architectural issues, with the aim of making the two functionally equivalent.
+ *
+ * Implemented features:
+ * * RTSP 1.0 support
+ * * Lower transports: TCP, UDP, UDP-Multicast
+ * * RTCP SR and RTCP RR
+ * * RTCP-based A/V sync
+ * * Lower transport selection and priority (NEW!)
+ *   - Also supports different lower transports for each SETUP
+ *
+ * Some missing features:
+ * * SET_PARAMETER/GET_PARAMETER messages
+ * * SRTP support
+ * * VOD support: PAUSE, seeking, etc
+ * * ONVIF backchannel and trick mode support
+ * * and more
+ *
+ * Please see the [README](https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/blob/main/net/rtsp/README.md)
+ * for a complete and up-to-date list.
+ */
 use gst::glib;
 use gst::prelude::*;
 
