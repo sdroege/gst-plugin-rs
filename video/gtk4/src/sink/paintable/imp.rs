sdroege/gst-plugin-rs

commit 3423d05f77ae08787c0b3d188e372cf3ce16c913

gtk4: Do scaling with append_texture()
This is equivalent, but will be needed for the scaling filter support.

Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1309>

video/gtk4/src/sink/paintable/imp.rs
@@ -177,7 +177,6 @@ impl PaintableImpl for Paintable {
             );
 
             snapshot.translate(&graphene::Point::new(trans_x as f32, trans_y as f32));
-            snapshot.scale(scale_x as f32, scale_y as f32);
 
             for Texture {
                 texture,
@@ -191,7 +190,9 @@ impl PaintableImpl for Paintable {
             {
                 snapshot.push_opacity(*global_alpha as f64);
 
-                let bounds = graphene::Rect::new(*x, *y, *paintable_width, *paintable_height);
+                let texture_width = *paintable_width * scale_x as f32;
+                let texture_height = *paintable_height * scale_y as f32;
+                let bounds = graphene::Rect::new(*x, *y, texture_width, texture_height);
 
                 // Only premultiply GL textures that expect to be in premultiplied RGBA format.
                 //

commit 523a46b4f561e2dfac05158e489585d2a37ab5e8

gtk4: scale texture position
Fix regression in 0.12 introduced by 3423d05f77ae08787c0b3d188e372cf3ce16c913

Code from Ivan Molodetskikh suggested on Matrix.

Fix #519

Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1499>

video/gtk4/src/sink/paintable/imp.rs
@@ -220,7 +220,9 @@ impl PaintableImpl for Paintable {
 
                 let texture_width = *paintable_width * scale_x as f32;
                 let texture_height = *paintable_height * scale_y as f32;
-                let bounds = graphene::Rect::new(*x, *y, texture_width, texture_height);
+                let x = *x * scale_x as f32;
+                let y = *y * scale_y as f32;
+                let bounds = graphene::Rect::new(x, y, texture_width, texture_height);
 
                 // Only premultiply GL textures that expect to be in premultiplied RGBA format.
                 //