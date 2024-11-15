4e2c16a2afc3674fcd445b8dafef168f5929f82c
Taruntej Kanakamalla <taruntej@asymptotic.io>
2024-11-15 10:43:52 +0530

    Revert "threadshare: udp: avoid getifaddrs in android"
    This reverts commit 75a0baa6fa67f80fb391f0255877d21ea8d9eb7c.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/1918>

--- generic/threadshare/src/udpsrc/imp.rs
+++ generic/threadshare/src/udpsrc/imp.rs
@@ -39,10 +39,6 @@ use crate::socket::{wrap_socket, GioSocketWrapper, Socket, SocketError, SocketRe
 use futures::channel::mpsc::{channel, Receiver, Sender};
 use futures::pin_mut;
 
-//FIXME: Remove this when https://github.com/mmastrac/getifaddrs/issues/5 is fixed in the `getifaddrs` crate
-#[cfg(target_os = "android")]
-use net::getifaddrs;
-
 const DEFAULT_ADDRESS: Option<&str> = Some("0.0.0.0");
 const DEFAULT_PORT: i32 = 5004;
 const DEFAULT_REUSE: bool = true;
@@ -378,46 +374,42 @@ impl TaskImpl for UdpSrcTask {
                     let multi_ifaces: Vec<String> =
                         multicast_iface.split(',').map(|s| s.to_string()).collect();
 
-                    //FIXME: Remove this when https://github.com/mmastrac/getifaddrs/issues/5 is fixed in the `getifaddrs` crate
-                    #[cfg(not(target_os = "android"))]
-                    {
-                        let iter = getifaddrs::getifaddrs().map_err(|err| {
-                            gst::error_msg!(
-                                gst::ResourceError::OpenRead,
-                                ["Failed to get interfaces: {}", err]
-                            )
-                        })?;
+                    let iter = getifaddrs::getifaddrs().map_err(|err| {
+                        gst::error_msg!(
+                            gst::ResourceError::OpenRead,
+                            ["Failed to get interfaces: {}", err]
+                        )
+                    })?;
 
-                        iter.for_each(|iface| {
-                            let ip_ver = if iface.address.is_ipv4() {
-                                "IPv4"
+                    iter.for_each(|iface| {
+                        let ip_ver = if iface.address.is_ipv4() {
+                            "IPv4"
+                        } else {
+                            "IPv6"
+                        };
+
+                        for m in &multi_ifaces {
+                            if &iface.name == m {
+                                self.multicast_ifaces.push(iface.clone());
+                                gst::debug!(
+                                    CAT,
+                                    obj = self.element,
+                                    "Interface {m} available, version: {ip_ver}"
+                                );
                             } else {
-                                "IPv6"
-                            };
-
-                            for m in &multi_ifaces {
-                                if &iface.name == m {
+                                // check if name matches the interface description (Friendly name) on Windows
+                                #[cfg(windows)]
+                                if &iface.description == m {
                                     self.multicast_ifaces.push(iface.clone());
                                     gst::debug!(
                                         CAT,
                                         obj = self.element,
                                         "Interface {m} available, version: {ip_ver}"
                                     );
-                                } else {
-                                    // check if name matches the interface description (Friendly name) on Windows
-                                    #[cfg(windows)]
-                                    if &iface.description == m {
-                                        self.multicast_ifaces.push(iface.clone());
-                                        gst::debug!(
-                                            CAT,
-                                            obj = self.element,
-                                            "Interface {m} available, version: {ip_ver}"
-                                        );
-                                    }
                                 }
                             }
-                        });
-                    }
+                        }
+                    });
                 }
 
                 if self.multicast_ifaces.is_empty() {
