a6ce185740b9438f530586ef7070ac8de2f3e05a
Sebastian Dröge <sebastian@centricular.com>
2025-04-01 19:46:14 +0300

    mp4mux: Don't write composition time offsets if they're all zero
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2169>

--- mux/mp4/src/mp4mux/boxes.rs
+++ mux/mp4/src/mp4mux/boxes.rs
@@ -645,7 +645,7 @@ fn write_stbl(
             if composition_time_offset < 0 {
                 need_ctts = Some(1);
                 break;
-            } else {
+            } else if composition_time_offset != 0 {
                 need_ctts = Some(0);
             }
         }
