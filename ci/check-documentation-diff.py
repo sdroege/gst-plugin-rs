3c830e7ea88489d24c7d591466d85505dcdb313d
Guillaume Desmottes <guillaume.desmottes@onestream.live>
2025-05-27 08:05:45 +0200

    ci: fix check-documentation-diff curl command
    Need to pass -L to follow the moved location.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2262>

--- ci/check-documentation-diff.py
+++ ci/check-documentation-diff.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         with open(diffname, 'w') as diff:
             subprocess.check_call(['git', 'diff'], stdout=diff)
             print('\033[91mYou have a diff in the documentation cache. Please update with:\033[0m')
-            print('     $ curl %s/%s | git apply -' % (os.environ['CI_ARTIFACTS_URL'], diffname.replace('../', '')))
+            print('     $ curl -L %s/%s | git apply -' % (os.environ['CI_ARTIFACTS_URL'], diffname.replace('../', '')))
 
     if res != 0:
         print('(note that it might take a few minutes for artefacts to be available on the server)\n')
