14f7401c9e89e21e2b5401828749fe9d7d7f8688
Matthew Waters <matthew@centricular.com>
2025-06-05 17:21:32 +1000

    cea708mux: use cea708-types push_code_into_single_service
    Simplifies the code significantly as we don't need to track whether a service or
    a packet is full or not.
    
    Fixes at least one case of a WouldOverflow panic and a subtraction underflow.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2277>

--- video/closedcaption/tests/cea708mux.rs
+++ video/closedcaption/tests/cea708mux.rs
@@ -6,6 +6,8 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
+use std::collections::HashMap;
+
 use gst::prelude::*;
 use pretty_assertions::assert_eq;
 
@@ -22,14 +24,14 @@ fn init() {
     });
 }
 
-fn gen_cc_data(seq: u8, service: u8, codes: &[Code]) -> gst::Buffer {
+fn gen_cc_data(seq: u8, service_no: u8, codes: &[Code]) -> gst::Buffer {
     assert!(seq < 4);
-    assert!(service < 64);
+    assert!(service_no < 64);
 
     let fps = Framerate::new(30, 1);
     let mut writer = CCDataWriter::default();
     let mut packet = DTVCCPacket::new(seq);
-    let mut service = Service::new(service);
+    let mut service = Service::new(service_no);
     for c in codes {
         service.push_code(c).unwrap();
     }
@@ -37,7 +39,7 @@ fn gen_cc_data(seq: u8, service: u8, codes: &[Code]) -> gst::Buffer {
     writer.push_packet(packet);
     let mut data = vec![];
     writer.write(fps, &mut data).unwrap();
-    println!("generated {data:x?}");
+    println!("generated {seq} for service {service_no} {data:x?}");
     let data = data.split_off(2);
     let mut buf = gst::Buffer::from_mut_slice(data);
     {
@@ -228,5 +230,139 @@ fn test_cea708mux_inputs_overflow_output() {
         // limits of the packet.
         assert!((30..=31).contains(&codes.len()));
         assert_eq!(&codes[..30], &CODES[no as usize..no as usize + 30]);
+    }
+}
+#[test]
+fn test_cea708mux_inputs_overflow_output_new_service() {
+    init();
+
+    static CODES: [Code; 46] = [
+        Code::LatinLowerA,
+        Code::LatinLowerB,
+        Code::LatinLowerC,
+        Code::LatinLowerD,
+        Code::LatinLowerE,
+        Code::LatinLowerF,
+        Code::LatinLowerG,
+        Code::LatinLowerH,
+        Code::LatinLowerI,
+        Code::LatinLowerJ,
+        Code::LatinLowerK,
+        Code::LatinLowerL,
+        Code::LatinLowerM,
+        Code::LatinLowerN,
+        Code::LatinLowerO,
+        Code::LatinLowerP,
+        Code::LatinLowerQ,
+        Code::LatinLowerR,
+        Code::LatinLowerS,
+        Code::LatinLowerT,
+        Code::LatinLowerU,
+        Code::LatinLowerV,
+        Code::LatinLowerW,
+        Code::LatinLowerX,
+        Code::LatinLowerY,
+        Code::LatinLowerZ,
+        Code::LatinCapitalA,
+        Code::LatinCapitalB,
+        Code::LatinCapitalC,
+        Code::LatinCapitalD,
+        Code::LatinCapitalE,
+        Code::LatinCapitalF,
+        Code::LatinCapitalG,
+        Code::LatinCapitalH,
+        Code::LatinCapitalI,
+        Code::LatinCapitalJ,
+        Code::LatinCapitalK,
+        Code::LatinCapitalL,
+        Code::LatinCapitalM,
+        Code::LatinCapitalN,
+        Code::LatinCapitalO,
+        Code::LatinCapitalP,
+        Code::LatinCapitalQ,
+        Code::LatinCapitalR,
+        Code::LatinCapitalS,
+        Code::LatinCapitalT,
+    ];
+
+    let mut h = gst_check::Harness::with_padnames("cea708mux", None, Some("src"));
+    let mut sinks = (0..6)
+        .map(|idx| {
+            let mut sink = gst_check::Harness::with_element(
+                &h.element().unwrap(),
+                Some(&format!("sink_{idx}")),
+                None,
+            );
+            sink.set_src_caps_str("closedcaption/x-cea-708,format=cc_data,framerate=60/1");
+            sink
+        })
+        .collect::<Vec<_>>();
+
+    let eos = gst::event::Eos::new();
+
+    for (i, sink) in sinks.iter_mut().enumerate() {
+        let buf = gen_cc_data(0, i as u8 + 1, &CODES[i..i + 30]);
+        sink.push(buf).unwrap();
+    }
+    for (i, sink) in sinks.iter_mut().enumerate() {
+        let i = 5 - i;
+        let buf = gen_cc_data(1, i as u8 + 1, &CODES[i + 6..i + 36]);
+        sink.push(buf).unwrap();
+        sink.push_event(eos.clone());
+    }
+
+    let mut parser = CCDataParser::new();
+    let mut seen_services: HashMap<u8, Vec<Code>, _> = HashMap::new();
+    let mut parsed_sequence_no = 0;
+    loop {
+        let mut parsed_packet = None;
+        while parsed_packet.is_none() {
+            let out = h.pull().unwrap();
+            let readable = out.map_readable().unwrap();
+            let mut cc_data = vec![0; 2];
+            cc_data[0] = 0x80 | 0x40 | ((readable.len() / 3) & 0x1f) as u8;
+            cc_data[1] = 0xFF;
+            cc_data.extend(readable.iter());
+            println!("pushed {cc_data:x?}");
+            parser.push(&cc_data).unwrap();
+            println!("parser: {parser:x?}");
+            parsed_packet = parser.pop_packet();
+        }
+        let parsed_packet = parsed_packet.unwrap();
+        println!("parsed: {parsed_packet:?}");
+        assert_eq!(parsed_packet.sequence_no(), parsed_sequence_no);
+        parsed_sequence_no += 1;
+        let services = parsed_packet.services();
+        // TODO: deterministic service ordering?
+        for service in services {
+            assert!((1..=6).contains(&service.number()));
+            seen_services
+                .entry(service.number())
+                .and_modify(|entry| entry.extend(service.codes().iter().cloned()))
+                .or_insert(service.codes().to_vec());
+        }
+        for (service_no, codes) in seen_services.iter() {
+            println!(
+                "seen service: {service_no}, codes (len: {}): {codes:?}",
+                codes.len()
+            );
+        }
+        if seen_services.keys().len() >= 6 && seen_services.values().all(|svc| svc.len() >= 60) {
+            break;
+        }
+    }
+
+    let mut service_numbers = seen_services.keys().copied().collect::<Vec<_>>();
+    service_numbers.sort();
+    assert_eq!(service_numbers, (1..=6).collect::<Vec<_>>());
+    for no in service_numbers {
+        let codes = seen_services.get(&no).unwrap();
+        println!("service {no}: {:?}", codes);
+        let offset = no as usize - 1;
+        // one of the services will have a length that is 1 byte shorter than others due to size
+        // limits of the packet.
+        assert_eq!(60, codes.len());
+        assert_eq!(&codes[..30], &CODES[offset..offset + 30]);
+        assert_eq!(&codes[30..60], &CODES[offset + 6..offset + 36]);
     }
 }
