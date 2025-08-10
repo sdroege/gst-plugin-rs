2ac1d952b94e6fb35bd1a9b4fcd55f3a4c92dd2b
Sebastian Dröge <sebastian@centricular.com>
2025-08-10 12:36:41 +0300

    Update to async-tungstenite 0.31
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2449>

--- net/webrtc/src/signaller/imp.rs
+++ net/webrtc/src/signaller/imp.rs
@@ -207,7 +207,7 @@ impl Signaller {
         gst::info!(CAT, imp = self, "connected");
 
         // Channel for asynchronously sending out websocket message
-        let (ws_sink, mut ws_stream) = ws.split();
+        let (mut ws_sink, mut ws_stream) = ws.split();
 
         // 1000 is completely arbitrary, we simply don't want infinite piling
         // up of messages as with unbounded
