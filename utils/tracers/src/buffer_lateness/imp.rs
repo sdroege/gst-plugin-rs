sdroege/gst-plugin-rs

commit 63a2b847f10029a5d5875c380f7889b6bbd7c8b8

tracers: Fix inverted append logic when writing log files
The most annoying side effect of this was that old logs from previous runs would
get appended to instead of starting fresh, creating some very confusing graphs :)

Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2625>

utils/tracers/src/buffer_lateness/imp.rs
@@ -170,10 +170,10 @@ impl BufferLateness {
 
         let mut state = self.state.lock().unwrap();
         let path = file_path.map_or_else(|| state.settings.file.clone(), PathBuf::from);
-        let first_write = state.logs_written.contains(&path);
+        let should_append = state.logs_written.contains(&path);
 
         let mut file = match std::fs::OpenOptions::new()
-            .append(!first_write)
+            .append(should_append)
             .create(true)
             .open(path.clone())
         {

utils/tracers/src/queue_levels/imp.rs
@@ -565,10 +565,10 @@ impl QueueLevels {
 
         let mut state = self.state.lock().unwrap();
         let path = file_path.map_or_else(|| state.settings.file.clone(), PathBuf::from);
-        let first_write = state.logs_written.contains(&path);
+        let should_append = state.logs_written.contains(&path);
 
         let mut file = match std::fs::OpenOptions::new()
-            .append(!first_write)
+            .append(should_append)
             .create(true)
             .open(path.clone())
         {

commit 2b7f4d57421628a6ac19b10524e91cf055527b31

tracers: Fix inability to create new log files
Regressed in 63a2b847f10029a5d5875c380f7889b6bbd7c8b8.

That commit fixed appending logic but missed adding a write(true) when not appending,
so our tracers were unable to create any new files...

Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2640>

utils/tracers/src/buffer_lateness/imp.rs
@@ -170,16 +170,18 @@ impl BufferLateness {
 
         let mut state = self.state.lock().unwrap();
         let path = file_path.map_or_else(|| state.settings.file.clone(), PathBuf::from);
-        let should_append = state.logs_written.contains(&path);
+        let first_write = !state.logs_written.contains(&path);
 
         let mut file = match std::fs::OpenOptions::new()
-            .append(should_append)
             .create(true)
-            .open(path.clone())
+            .write(true)
+            .truncate(first_write)
+            .append(!first_write)
+            .open(&path)
         {
             Ok(file) => file,
             Err(err) => {
-                gst::error!(CAT, imp = self, "Failed to create file: {err}");
+                gst::error!(CAT, imp = self, "Failed to open file: {err}");
                 return;
             }
         };

utils/tracers/src/queue_levels/imp.rs
@@ -565,16 +565,18 @@ impl QueueLevels {
 
         let mut state = self.state.lock().unwrap();
         let path = file_path.map_or_else(|| state.settings.file.clone(), PathBuf::from);
-        let should_append = state.logs_written.contains(&path);
+        let first_write = !state.logs_written.contains(&path);
 
         let mut file = match std::fs::OpenOptions::new()
-            .append(should_append)
             .create(true)
-            .open(path.clone())
+            .write(true)
+            .truncate(first_write)
+            .append(!first_write)
+            .open(&path)
         {
             Ok(file) => file,
             Err(err) => {
-                gst::error!(CAT, imp = self, "Failed to create file: {err}");
+                gst::error!(CAT, imp = self, "Failed to open file: {err}");
                 return;
             }
         };