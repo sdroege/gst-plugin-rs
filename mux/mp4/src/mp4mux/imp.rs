64dfa07a8b190c5df28933850898bfdae98e6c99
Sebastian Dröge <sebastian@centricular.com>
2025-11-17 09:40:58 +0200

    isobmff: mp4mux: Drop state lock before requesting a pad
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2696>

--- mux/mp4/src/mp4mux/imp.rs
+++ mux/mp4/src/mp4mux/imp.rs
@@ -1849,6 +1849,8 @@ impl ElementImpl for MP4Mux {
             );
             return None;
         }
+        drop(state);
+
         let pad = self.parent_request_new_pad(templ, name, caps);
 
         if let Some(ref pad) = pad {
