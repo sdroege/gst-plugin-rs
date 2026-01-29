41aeac43f12d6e6d040b31e4918c030213a50579
Sebastian Dröge <sebastian@centricular.com>
2026-01-29 19:52:00 +0200

    rtprecv: Don't panic if no buffers of a bufferlist can be directly forwarded
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2846>

--- net/rtp/src/rtpbin2/rtprecv.rs
+++ net/rtp/src/rtpbin2/rtprecv.rs
@@ -1469,7 +1469,7 @@ impl RtpRecv {
             });
             state = maybe_state.unwrap();
             ret?;
-        } else {
+        } else if !list.is_empty() {
             state = self.handle_push_jitterbuffer(
                 state,
                 id,
