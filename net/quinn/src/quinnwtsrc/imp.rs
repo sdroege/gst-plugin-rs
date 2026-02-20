8d6f809a4e93b47551a257db0013c518eeff4640
Sebastian Dröge <sebastian@centricular.com>
2026-02-20 22:48:14 +0200

    quinn: Work around minor quinn API breakage
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/2892>

--- net/quinn/src/quinnwtsrc/imp.rs
+++ net/quinn/src/quinnwtsrc/imp.rs
@@ -328,7 +328,9 @@ impl ObjectImpl for QuinnWebTransportSrc {
             "stats" => {
                 let state = self.state.lock().unwrap();
                 match *state {
-                    State::Started(ref state) => get_stats(Some(state.session.stats())).to_value(),
+                    State::Started(ref state) => {
+                        get_stats(Some((**state.session).stats())).to_value()
+                    }
                     State::Stopped => get_stats(None).to_value(),
                 }
             }
