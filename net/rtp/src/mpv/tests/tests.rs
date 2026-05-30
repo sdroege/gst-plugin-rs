3c5506d55cf7f6e200acdfcb2a76909fc16f5b09
Tim-Philipp Müller <tim@centricular.com>
2026-05-30 17:47:38 +0200

    rtp2: mpv: add MPEG-1 payloading/depayloading unit test
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/3092>

--- net/rtp/src/mpv/tests/tests.rs
+++ net/rtp/src/mpv/tests/tests.rs
@@ -25,14 +25,6 @@ fn init() {
 // gst-launch-1.0 videotestsrc num-buffers=2
 //  ! video/x-raw,framerate=50/1
 //  ! timeoverlay font-desc=Sans,36
-//  ! mpeg2enc
-//  ! filesink location=vts-320x240_mpeg1.mpv
-//
-// FIXME: unused - const MPV_DATA: &[u8] = include_bytes!("vts-320x240-mpeg1.mpv").as_slice();
-
-// gst-launch-1.0 videotestsrc num-buffers=2
-//  ! video/x-raw,framerate=50/1
-//  ! timeoverlay font-desc=Sans,36
 //  ! avenc_mpeg2video bitrate=5000
 //  ! filesink location=vts-320x240_mpeg2.mpv
 //
@@ -54,7 +46,7 @@ fn make_buffer(
     buf
 }
 
-// Check basic MPV payloading/depayloading
+// Check basic MPEG-2 MPV payloading/depayloading
 //
 #[test]
 #[allow(clippy::vec_init_then_push)]
@@ -230,6 +222,166 @@ fn rtp_mpv_pay_depay_mpeg2_simple() {
         let expected_ts = match i {
             0..=18 => gst::ClockTime::ZERO,
             19.. => gst::ClockTime::from_mseconds(20),
+        };
+        expected_depay.push(vec![
+            ExpectedBuffer::builder()
+                .pts(expected_ts)
+                .size(plen)
+                .flags(expected_flags)
+                .build(),
+        ]);
+    }
+
+    run_test_pipeline(
+        Source::Buffers(input_caps, input_buffers),
+        "rtpmpvpay2",
+        "rtpmpvdepay2",
+        expected_pay,
+        expected_depay,
+    );
+}
+
+// gst-launch-1.0 videotestsrc num-buffers=2
+//  ! video/x-raw,framerate=50/1
+//  ! timeoverlay font-desc=Sans,36
+//  ! mpeg2enc
+//  ! filesink location=vts-320x240_mpeg1.mpv
+//
+const MPV1_DATA: &[u8] = include_bytes!("vts-320x240-mpeg1.mpv").as_slice();
+
+// Check basic MPEG-1 MPV payloading/depayloading
+//
+#[test]
+#[allow(clippy::vec_init_then_push)]
+fn rtp_mpv_pay_depay_mpeg1_simple() {
+    init();
+
+    let packets = parse_packets_from_slice(MPV1_DATA).expect("packets");
+
+    let second_pic_hdr = packets
+        .iter()
+        .filter(|p| p.ptype() == PacketType::Picture)
+        .nth(1)
+        .expect("second frame");
+
+    let (frame1_packets, frame2_packets) = packets.split_at(second_pic_hdr.index());
+
+    let mut frame1 = vec![];
+    for p in frame1_packets {
+        frame1.extend_from_slice(p.data(MPV1_DATA));
+    }
+
+    let mut frame2 = vec![];
+    for p in frame2_packets {
+        frame2.extend_from_slice(p.data(MPV1_DATA));
+    }
+
+    let input_caps = {
+        gst::Caps::builder("video/mpeg")
+            .field("systemstream", false)
+            .field("mpegversion", 1)
+            .field("width", 320)
+            .field("height", 240)
+            .field("framerate", gst::Fraction::new(50, 1))
+            .field("parsed", true)
+            .build()
+    };
+
+    let mut input_buffers = vec![];
+
+    input_buffers.push(make_buffer(
+        frame1,
+        gst::ClockTime::from_mseconds(0),
+        gst::ClockTime::from_mseconds(20),
+        gst::BufferFlags::DISCONT,
+    ));
+
+    input_buffers.push(make_buffer(
+        frame2,
+        gst::ClockTime::from_mseconds(20),
+        gst::ClockTime::from_mseconds(20),
+        gst::BufferFlags::empty(),
+    ));
+
+    let mut expected_pay = vec![];
+
+    // First frame is payloaded into 7 RTP packets
+    {
+        let mut expected_rtp_packet_list = vec![];
+
+        expected_rtp_packet_list.push(
+            ExpectedPacket::builder()
+                .pts(gst::ClockTime::ZERO)
+                .flags(gst::BufferFlags::DISCONT)
+                .pt(32)
+                .rtp_time(0)
+                .build(),
+        );
+
+        for _ in 1..6 {
+            expected_rtp_packet_list.push(
+                ExpectedPacket::builder()
+                    .pts(gst::ClockTime::ZERO)
+                    .flags(gst::BufferFlags::empty())
+                    .pt(32)
+                    .rtp_time(0)
+                    .build(),
+            );
+        }
+
+        expected_rtp_packet_list.push(
+            ExpectedPacket::builder()
+                .pts(gst::ClockTime::ZERO)
+                .flags(gst::BufferFlags::MARKER)
+                .marker_bit(true)
+                .pt(32)
+                .rtp_time(0)
+                .build(),
+        );
+
+        expected_pay.push(expected_rtp_packet_list);
+    }
+
+    // Second is payloaded into 2 RTP packets
+    {
+        expected_pay.push(
+            [
+                ExpectedPacket::builder()
+                    .pts(gst::ClockTime::from_mseconds(20))
+                    .flags(gst::BufferFlags::empty())
+                    .pt(32)
+                    .rtp_time(1800)
+                    .build(),
+                ExpectedPacket::builder()
+                    .pts(gst::ClockTime::from_mseconds(20))
+                    .flags(gst::BufferFlags::MARKER)
+                    .marker_bit(true)
+                    .pt(32)
+                    .rtp_time(1800)
+                    .build(),
+            ]
+            .to_vec(),
+        );
+    }
+
+    let mut expected_depay = vec![];
+
+    for (i, &plen) in [
+        // Frame 1
+        496, 1107, 1003, 435, 767, 994, 488, // Frame 2
+        929, 693,
+    ]
+    .iter()
+    .enumerate()
+    {
+        let expected_flags = match i {
+            0 => gst::BufferFlags::DISCONT,
+            6 | 8 => gst::BufferFlags::MARKER, // End of frame
+            _ => gst::BufferFlags::empty(),
+        };
+        let expected_ts = match i {
+            0..=6 => gst::ClockTime::ZERO,
+            7.. => gst::ClockTime::from_mseconds(20),
         };
         expected_depay.push(vec![
             ExpectedBuffer::builder()
