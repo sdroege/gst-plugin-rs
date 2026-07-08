00462aa9989e9769e610e3f8be96fc6cbdb46346
Emiel Van Severen <emiel@otiv.ai>
2026-07-08 22:32:39 +0200

    webrtc: add support for nvv4l2h265enc
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/3160>

--- net/webrtc/src/webrtcsink/imp.rs
+++ net/webrtc/src/webrtcsink/imp.rs
@@ -817,7 +817,7 @@ fn configure_encoder(enc: &gst::Element, start_bitrate: u32) {
                 enc.set_property("key-int-max", 256u32);
                 enc.set_property_from_str("rate-control", "cbr");
             }
-            "nvv4l2h264enc" => {
+            "nvv4l2h264enc" | "nvv4l2h265enc" => {
                 enc.set_property("bitrate", start_bitrate);
                 if enc.has_property("preset-level") {
                     enc.set_property_from_str("preset-level", "UltraFastPreset");
@@ -1176,6 +1176,7 @@ impl VideoEncoder {
                 | "vaapivp9enc"
                 | "qsvh264enc"
                 | "nvv4l2h264enc"
+                | "nvv4l2h265enc"
                 | "nvv4l2vp8enc"
                 | "nvv4l2vp9enc"
                 | "nvav1enc"
@@ -1207,9 +1208,8 @@ impl VideoEncoder {
             | "vah264lpenc" | "vah265enc" | "vah265lpenc" | "vaav1enc" | "vaav1lpenc" => {
                 (self.element.property::<u32>("bitrate") * 1000) as i32
             }
-            "openh264enc" | "nvv4l2h264enc" | "nvv4l2vp8enc" | "nvv4l2vp9enc" | "nvv4l2av1enc" => {
-                (self.element.property::<u32>("bitrate")) as i32
-            }
+            "openh264enc" | "nvv4l2h264enc" | "nvv4l2h265enc" | "nvv4l2vp8enc" | "nvv4l2vp9enc"
+            | "nvv4l2av1enc" => (self.element.property::<u32>("bitrate")) as i32,
             "rav1enc" => self.element.property::<i32>("bitrate"),
             _ => return Err(WebRTCSinkError::BitrateNotSupported),
         };
@@ -1249,9 +1249,8 @@ impl VideoEncoder {
                 self.element
                     .set_property("bitrate", (bitrate / 1000) as u32);
             }
-            "openh264enc" | "nvv4l2h264enc" | "nvv4l2vp8enc" | "nvv4l2vp9enc" | "nvv4l2av1enc" => {
-                self.element.set_property("bitrate", bitrate as u32)
-            }
+            "openh264enc" | "nvv4l2h264enc" | "nvv4l2h265enc" | "nvv4l2vp8enc" | "nvv4l2vp9enc"
+            | "nvv4l2av1enc" => self.element.set_property("bitrate", bitrate as u32),
             "rav1enc" => self.element.set_property("bitrate", bitrate),
             _ => return Err(WebRTCSinkError::BitrateNotSupported),
         }
