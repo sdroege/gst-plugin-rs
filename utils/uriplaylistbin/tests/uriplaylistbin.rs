77d1627aab51cf929fe96e4843efdcad5cf5a74a
Sanchayan Maity <sanchayan@centricular.com>
2026-07-21 21:23:57 +0530

    Switch to aws-lc-rs as default
    We also introduce rustls-aws-lc-rs and rustls-ring as feature flags on
    crates using dependencies which depend on ring or aws-lc-rs directly or
    indirectly. This should allow users to depend on one of ring or aws-lc-rs
    as required.
    
    Some dependencies can still pull in ring in their dependency tree. For
    example, burn depends on an old version of reqwest and till that is
    updated, cargo tree will continue to show ring. Similarly for spotify.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/3273>

--- utils/uriplaylistbin/tests/uriplaylistbin.rs
+++ utils/uriplaylistbin/tests/uriplaylistbin.rs
@@ -555,7 +555,16 @@ fn infinite_to_finite() {
 fn cache() {
     let media = TestMedia::mkv_http();
 
-    let _ = rustls::crypto::ring::default_provider().install_default();
+    #[cfg(all(feature = "rustls-ring", not(feature = "rustls-aws-lc-rs")))]
+    {
+        let _ = rustls::crypto::ring::default_provider().install_default();
+    }
+    #[cfg(feature = "rustls-aws-lc-rs")]
+    {
+        let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
+    }
+    #[cfg(all(not(feature = "rustls-ring"), not(feature = "rustls-aws-lc-rs")))]
+    compile_error!("Either rustls-ring or rustls-aws-lc-rs feature must be enabled");
 
     if let Err(err) = reqwest::blocking::get(&media.uri) {
         println!("skipping test as {} is not available: {}", media.uri, err);
