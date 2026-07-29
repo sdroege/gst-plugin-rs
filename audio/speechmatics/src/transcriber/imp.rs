e1d4b815518b5a2a5ba9ebe3a08fcf5ed4c7bb0e
Sebastian Dröge <sebastian@centricular.com>
2026-07-29 16:04:39 +0300

    Fix a couple of new Rust 1.97 clippy warnings
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/3198>

--- audio/speechmatics/src/transcriber/imp.rs
+++ audio/speechmatics/src/transcriber/imp.rs
@@ -1514,7 +1514,7 @@ impl Transcriber {
                     async_tungstenite::tungstenite::handshake::client::generate_key(),
                 )
                 .header("Sec-Websocket-Version", "13")
-                .header("Authorization", format!("Bearer {}", &api_key))
+                .header("Authorization", format!("Bearer {}", api_key))
                 .body(())
                 .unwrap();
 
