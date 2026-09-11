0d3616f38a46d2225574b00fea21450b336ce355
Sanchayan Maity <sanchayan@centricular.com>
2026-09-11 12:57:02 +0530

    Reapply "ci: Exclude plugins depending on crypto providers with --no-default-features"
    This reverts commit 039c99251a173e6a752d11ae710fad00cd6765e1.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gst-plugins-rs/-/merge_requests/3255>

--- ci/run-clippy.sh
+++ ci/run-clippy.sh
@@ -9,6 +9,17 @@ cargo clippy --version
 cpus=$(nproc || sysctl -n hw.ncpu)
 CARGO_FLAGS="--color=always -j${FDO_CI_CONCURRENT:-$cpus}"
 
-for cfg in "" "--all-features --exclude gst-plugin-gtk4 --exclude gst-plugin-whisper --exclude gst-plugin-llamacpp" "--no-default-features --exclude gst-plugin-burn --exclude gst-plugin-compress"; do
+no_default_excludes="\
+    --exclude gst-plugin-burn \
+    --exclude gst-plugin-compress \
+    --exclude gst-plugin-webrtc \
+    --exclude gst-plugin-reqwest \
+    --exclude gst-plugin-icecast \
+    --exclude gst-plugin-rtsp \
+    --exclude gst-plugin-quinn \
+    --exclude gst-plugin-webrtc-signalling \
+    --exclude gst-plugin-uriplaylistbin"
+
+for cfg in "" "--all-features --exclude gst-plugin-gtk4 --exclude gst-plugin-whisper --exclude gst-plugin-llamacpp" "--no-default-features $no_default_excludes"; do
     cargo clippy $CARGO_FLAGS --locked --all --all-targets $cfg -- $CLIPPY_LINTS
 done
